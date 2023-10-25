cff27c4d634e53c9f59e0f94df068fb4c55b9b81
mlerena <manuellerena@gmail.com>
2023-10-25 17:26:08 -0300

    Music controls foreground services

--- src/android/MusicControlsNotificationKiller.java
+++ src/android/MusicControlsNotificationKiller.java
@@ -36,7 +36,7 @@ public class MusicControlsNotificationKiller extends Service {
 	}
 
 	public void setForeground(Notification notification) {
-	    Log.v("MusicControls", "setForeground " + 7824);
+	    Log.v("MusicControls", "setForeground " + this.notificationID);
 		this.startForeground(7824, notification);
 	}
 
