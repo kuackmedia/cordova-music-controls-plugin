c65f0025c84f8d713cbe284803d7ae5cb3cd3143
mlerena <manuellerena@gmail.com>
2024-08-08 10:42:08 -0300

    create Notification channel

--- src/android/MusicControlsNotificationKiller.java
+++ src/android/MusicControlsNotificationKiller.java
@@ -4,60 +4,78 @@ import android.app.Notification;
 import android.app.Service;
 import android.os.Build;
 import android.os.IBinder;
-import android.os.Binder;
 import android.app.NotificationManager;
 import android.content.Intent;
+import androidx.core.app.NotificationCompat;
 import android.util.Log;
 
 public class MusicControlsNotificationKiller extends Service {
 
-	private static int NOTIFICATION_ID;
-	private NotificationManager mNM;
-	private final IBinder mBinder = new KillBinder(this);
+    private static int NOTIFICATION_ID;
+    private NotificationManager mNM;
+    private final IBinder mBinder = new KillBinder(this);
+    private static final String CHANNEL_ID = "music_controls_channel";
 
-	@Override
-	public IBinder onBind(Intent intent) {
-		this.NOTIFICATION_ID=intent.getIntExtra("notificationID",1);
-		return mBinder;
-	}
-	@Override
-	public int onStartCommand(Intent intent, int flags, int startId) {
-		return Service.START_STICKY;
-	}
+    @Override
+    public IBinder onBind(Intent intent) {
+        this.NOTIFICATION_ID = intent.getIntExtra("notificationID", 1);
+        return mBinder;
+    }
 
-	@Override
-	public void onCreate() {
-		this.removeNotification();
-	}
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return Service.START_STICKY;
+    }
 
-	@Override
-	public void onDestroy() {
-		this.removeNotification();
-	}
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        this.removeNotification();
+    }
 
-	public void setForeground(Notification notification) {
-	    if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
-         //   return;
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        this.removeNotification();
+    }
+
+    public void setForeground(Notification notification) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel();
         }
-		this.startForeground(NOTIFICATION_ID, notification);
-	}
+        this.startForeground(NOTIFICATION_ID, notification);
+    }
 
-	public void clearForeground() {
-		if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
-			return;
-		}
+    public void clearForeground() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            this.stopForeground(STOP_FOREGROUND_DETACH);
+        }
+    }
 
-		this.stopForeground(STOP_FOREGROUND_DETACH);
-	}
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        this.removeNotification();
+        this.stopSelf();
+    }
 
-	@Override
-	public void onTaskRemoved(Intent rootIntent) {
-		this.removeNotification();
-		this.stopSelf();
-	}
+    private void removeNotification() {
+        mNM = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (mNM != null) {
+            mNM.cancel(NOTIFICATION_ID);
+        }
+    }
 
-	private void removeNotification() {
-		mNM = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-		mNM.cancel(NOTIFICATION_ID);
-	}
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Music Controls Service Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            mNM = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            if (mNM != null) {
+                mNM.createNotificationChannel(serviceChannel);
+            }
+        }
+    }
 }
